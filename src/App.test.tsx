1c598c2c260d28b558874732c0a2d258bae9f26c
Abhishek lohia <abhisheklohia@tripxl.com>
2025-12-06 13:51:16 +0530

    added

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { test, expect } from "vitest";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
+
+test("renders app", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(
+    screen.getByRole("heading", { name: /hii abhishek/i })
+  ).toBeInTheDocument();
+});
